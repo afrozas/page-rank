60da24e25132300a78c852aca00e86d86e232a77
enigmaeth <enigmaeth@gmail.com>
2017-10-18 14:46:31 +0530

    Adds reading JSON from file to dict

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
-from helpers.matrix_generator import MatrixGenerator
+from helpers.matrix_handler import MatrixHandler
 
 
 def run():
 	"""
 	driver function for page rank implementation
 	"""
-	MatrixGenerator.read_dataset_and_generate_matrix()
+	MatrixHandler.read_dataset_and_generate_matrix()
 
+	adjacency_list = MatrixHandler.load_adjacency_list_into_dict()
+	#print(adjacency_list)
 
 if __name__ == '__main__':
 	run()
