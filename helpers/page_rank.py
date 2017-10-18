8db838ea6dc0df5f0783cd615ac973c2d639c5da
enigmaeth <enigmaeth@gmail.com>
2017-10-18 23:00:41 +0530

    Adds Teleportation factor beta
    Random walk interpretation involving teleportation as Google's solution

--- helpers/page_rank.py
+++ helpers/page_rank.py
@@ -25,6 +25,7 @@ class PageRank:
 		page_rank_t1 = []
 		iteration = 10
 		itr = 0
+		beta = 0.85
 
 		for node in range(0, 281904):
 			page_rank_t.append(1/N)
@@ -33,24 +34,20 @@ class PageRank:
 		adjacency_list_outgoing = MatrixHandler.load_adjacency_list_outgoing()
 		adjacency_list_incoming = MatrixHandler.load_adjacency_list_incoming()
 
-		print("Page Rank for nodes numbered 1 through 10")
+		print("Page Rank for nodes numbered 1 through 1001")
 		while itr <= iteration:
 			# run power method for 'iteration' times
 			print("iteration: ", str(itr))
-			print(page_rank_t[1:11])
+			print(page_rank_t[1:1001])
 
 			for page in range(1, 281904):
 				pr_from_each_page = 0
 				page_rank_sum = 0
-				# if str(page) in adjacency_list_incoming:
 				for linked_page in adjacency_list_incoming[str(page)]:
-					#if str(linked_page) in adjacency_list_outgoing and linked_page in page_rank_t:
 					pr_from_each_page = page_rank_t[linked_page]/len(adjacency_list_outgoing[str(linked_page)])
-					# print("pr_from_each_page ", pr_from_each_page)
-					page_rank_sum = page_rank_sum + pr_from_each_page
-					# print(page_rank_sum)
-				# print("page_rank for page ", str(page), " is ", str(page_rank_sum))
-				page_rank_t1[page] = page_rank_sum
+					page_rank_sum = page_rank_sum + beta*pr_from_each_page
+
+				page_rank_t1[page] = page_rank_sum + (1-beta)/N
 
 			page_rank_t = page_rank_t1
 			itr = itr + 1
