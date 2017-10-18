8b6da53ae10883c507540d9c15b00240b929dd46
enigmaeth <enigmaeth@gmail.com>
2017-10-18 15:53:31 +0530

    Adds skeleton for PageRank implementation

---
+++ helpers/page_rank.py
@@ -0,0 +1,31 @@
+from helpers.matrix_handler import MatrixHandler
+
+
+class PageRank:
+	"""
+	Provides implementation of Google's PageRank algorithm
+	- Make the graph stochastic, irreducible and aperiodic (Markov Chains)
+	- PageRank Power Method
+	- Teleportation (handling spider traps and sink pages)
+	"""
+
+	def follow_Markov_Chain():
+		"""
+		preprocessor function to make the graph stochastic, irreducible and aperiodic 
+		"""
+		pass
+
+
+	def calculate_PageRank():
+		"""
+		provides PageRank core implementation
+		"""
+		pass
+
+
+	def allow_Teleports():
+		"""
+		adds teleportation factor to the PageRank equation
+		"""
+		pass
+		
